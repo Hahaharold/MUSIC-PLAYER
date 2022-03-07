016a3d08c5827109306b9c623a87184b79b0a322
HaroldJamesEnciso <100378606+Hahaharold@users.noreply.github.com>
2022-03-07 20:19:07 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,80 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Tahanan",
+    artist: "/by Adie/",
+    img: "music-1",
+    src: "Music-1"
+  },
+  {
+    name: "Paraluman",
+    artist: "/by Adie/",
+    img: "music-2",
+    src: "Music-2"
+  },
+  {
+    name: "Pagsamo",
+    artist: "/by Arthur Nery/",
+    img: "music-3",
+    src: "Music-3"
+  },
+  {
+    name: "Magbalik",
+    artist: "/by Callalily/",
+    img: "music-4",
+    src: "Music-4"
+  },
+  {
+    name: "Kay Tagal kitang Hinintay",
+    artist: "/by Agsunta/",
+    img: "music-5",
+    src: "Music-5"
+  },
+  {
+    name: "Binhi",
+    artist: "/by Arthur Nery/",
+    img: "music-6",
+    src: "Music-6"
+  },
+  {
+    name: "Higa",
+    artist: "/by Arthur Nery/",
+    img: "music-7",
+    src: "Music-7"
+  },
+  {
+    name: "Pagtingin",
+    artist: "/by Ben&Ben/",
+    img: "music-8",
+    src: "Music-8"
+  },
+  {
+    name: "Demonyo",
+    artist: "/by Agsunta/",
+    img: "music-9",
+    src: "Music-9"
+  },
+  {
+    name: "Synesthesia",
+    artist: "/by Agsunta/",
+    img: "music-10",
+    src: "Music-10"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
